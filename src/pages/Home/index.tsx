960df1536495554be3c5b876630f8240877b9ce3
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2021-10-28 10:46:17 -0300

    Add background image and logo. Adjusts the style of the Header component and the Home component. Adjust the initial component text and add icons to the buttons. Add global color variables.
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import { Header } from '../../components/Header';
 
+import { VscArrowRight, VscDeviceMobile } from 'react-icons/vsc';
+
 import style from './style.module.scss';
 
 export function Home() {
@@ -9,12 +11,16 @@ export function Home() {
       <Header />
       <main className={style.containerMain}>
         <div className={style.main}>
-          <h4>Crie ou acesses salas, para permitir a boa comunicação </h4>
+          <h4>Crie ou acesses salas para permitir a boa comunicação </h4>
           <h1>A plataforma que conecta você ao seu futuro local de estudo</h1>
         </div>
         <div className={style.buttonMain}>
-          <Link to="/">Acessar Plataforma</Link>
-          <Link to="/">Acessar Plataforma</Link>
+          <Link to="/">
+            Acessar Plataforma <VscArrowRight />{' '}
+          </Link>
+          <Link to="/">
+            Plataforma Mobile <VscDeviceMobile />
+          </Link>
         </div>
       </main>
     </>
