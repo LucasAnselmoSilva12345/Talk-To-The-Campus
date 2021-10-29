7c65c146282b52f26c83863cc045731960f8de8c
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2021-10-29 17:32:49 -0300

    Remove "Header" component, refactor the "Home" page, add "Main" component, with header, both with styles and responsive, and adjust styles in the responsiveness issue and the sidebar of the site
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,30 +1,11 @@
-import { Link } from 'react-router-dom';
-import { Header } from '../../components/Header';
+import { Main } from '../../components/Main';
 import { DesktopPresentation } from '../../components/DesktopPresentation';
-
-import { VscArrowRight, VscDeviceMobile } from 'react-icons/vsc';
-
-import style from './style.module.scss';
 import { DesktopApplication } from '../../components/DesktopApplication';
 
 export function Home() {
   return (
     <>
-      <Header />
-      <main className={style.containerMain}>
-        <div className={style.main}>
-          <h4>Crie ou acesses salas para permitir a boa comunicação </h4>
-          <h1>A plataforma que conecta você ao seu futuro local de estudo</h1>
-        </div>
-        <div className={style.buttonMain}>
-          <Link to="/">
-            Acessar Plataforma <VscArrowRight />{' '}
-          </Link>
-          <Link to="/">
-            Plataforma Mobile <VscDeviceMobile />
-          </Link>
-        </div>
-      </main>
+      <Main />
       <DesktopPresentation />
       <DesktopApplication />
     </>
