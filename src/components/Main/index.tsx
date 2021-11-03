55163abc6b83a579c1773deb175a6b8483546f77
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2021-11-03 11:23:38 -0300

    Add responsiveness to components, and remove react_fragament.
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -5,46 +5,42 @@ import styles from './style.module.scss';
 
 export function Main() {
   return (
-    <>
-      <main>
-        <header className={styles.containerGeneral}>
-          <div className={styles.container}>
-            <nav className={styles.navigation}>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/">Sobre</Link>
-                </li>
-                <li>
-                  <Link to="/">Equipe</Link>
-                </li>
-                <li>
-                  <Link to="/">Contato</Link>
-                </li>
-              </ul>
-            </nav>
+    <main className={styles.main}>
+      <header className={styles.containerGeneral}>
+        <div className={styles.container}>
+          <nav className={styles.navigation}>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/">Sobre</Link>
+              </li>
+              <li>
+                <Link to="/">Equipe</Link>
+              </li>
+              <li>
+                <Link to="/">Contato</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <section className={styles.containerSection}>
+          <div className={styles.section}>
+            <h4>talk to the campus</h4>
+            <h1>A plataforma que conecta você ao seu futuro local de estudo</h1>
+            <p>Crie ou acesses salas para permitir a boa comunicação</p>
           </div>
-          <section className={styles.containerSection}>
-            <div className={styles.section}>
-              <h4>talk to the campus</h4>
-              <h1>
-                A plataforma que conecta você ao seu futuro local de estudo
-              </h1>
-              <p>Crie ou acesses salas para permitir a boa comunicação</p>
-            </div>
-            <div className={styles.buttonSection}>
-              <Link to="/">
-                Acessar Plataforma <VscArrowRight />{' '}
-              </Link>
-              <Link to="/">
-                Plataforma Mobile <VscDeviceMobile />
-              </Link>
-            </div>
-          </section>
-        </header>
-      </main>
-    </>
+          <div className={styles.buttonSection}>
+            <Link to="/">
+              Acessar Plataforma <VscArrowRight />{' '}
+            </Link>
+            <Link to="/">
+              Plataforma Mobile <VscDeviceMobile />
+            </Link>
+          </div>
+        </section>
+      </header>
+    </main>
   );
 }
