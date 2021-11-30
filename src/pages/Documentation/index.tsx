6f3657ce019aa90a2e9f8a52cb9555f18e71ac87
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2021-11-30 18:27:58 -0300

    Adds text documentation.
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/Documentation/index.tsx
+++ src/pages/Documentation/index.tsx
@@ -12,77 +12,121 @@ export function Documentation() {
       </h1>
       <section className={styles.sectionDocumentation}>
         <div className={styles.rowDocumentation}>
+          <p>Política de Privacidade</p>
+
+          <p>Esta Política de Privacidade foi atualizada em 04/06/2020 </p>
+
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-            itaque sequi illo voluptate similique modi beatae tempora, illum
-            veritatis aut reprehenderit dolores omnis, ipsum accusamus pariatur
-            soluta maiores vel mollitia.
+            Esta Política de Privacidade sumariza nossas práticas de coleta e
+            tratamento de dados e descreve os direitos do usuário de acessar,
+            corrigir ou limitar o uso de seus dados pessoais por parte da Talk
+            to the Campus. Esta Política de Privacidade se aplica quando o
+            usuário visita ou usa o site.{' '}
           </p>
+
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            aspernatur, doloribus perferendis quam nemo eius laudantium
-            voluptatum. Assumenda, labore ipsum quo maxime expedita aliquid aut
-            cumque temporibus corrupti hic atque.
+            1. Coleta de Dados Coletamos alguns dados do usuário de forma
+            direta, como informações inseridas pelo próprio usuário em nossos
+            formulários e de forma automatizada, tais como informações sobre o
+            dispositivo do usuário e com quais páginas do nosso site o usuário
+            interage ou utiliza.{' '}
           </p>
+
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            aspernatur, doloribus perferendis quam nemo eius laudantium
-            voluptatum. Assumenda, labore ipsum quo maxime expedita aliquid aut
-            cumque temporibus corrupti hic atque.
+            1.1 Dados coletados diretamente: Quando o usuário se registra
+            através do nosso formulário e acessa nossa plataforma coletamos
+            todos os dados fornecidos diretamente, entre eles: Dados da conta:
+            Para usar determinados recursos (como assinar um Plano), é
+            necessário criar uma conta de usuário. Quando o usuário cria ou
+            atualiza sua conta, coletamos e armazenamos os dados fornecidos,
+            como nome completo, endereço físico, endereço de e-mail, senha e
+            atribuímos ao usuário um número de identificação exclusivo (“Dados
+            da conta”). Dados dos cursos: Quando o usuário assina um Plano,
+            coletamos alguns dados, como quais cursos foram iniciados e
+            concluídos pelo usuário. Dados sobre pagamento dos alunos: Quando o
+            usuário assina um Plano, coletamos alguns dados sobre a compra em
+            questão (tais como nome e CEP do usuário) quando necessário para
+            processar o pedido. Cabe ao usuário fornecer determinados dados
+            sobre pagamento e fatura diretamente aos nossos parceiros de
+            processamento de pagamentos, entre eles nome do usuário, informações
+            sobre o cartão de crédito, endereço de faturamento e CEP. Por
+            questões de segurança, a Talk to the Campus não coleta nem armazena
+            dados confidenciais do titular do cartão, tais como o número
+            completo do cartão de crédito ou os dados de autenticação do cartão.
+            Comunicações e suporte: Caso o usuário entre em contato para obter
+            suporte ou relatar um problema ou dúvida (independentemente de ter
+            criado uma conta), coletamos e armazenamos as informações de contato
+            do usuário, bem como mensagens e outros dados sobre o usuário, tais
+            como nome, endereço de e-mail, localização, sistema operacional,
+            endereço IP e quaisquer outros dados que o usuário forneça ou que
+            coletemos por meios automatizados (abordados abaixo). Estes dados
+            serão usados para responder ao usuário e pesquisar sobre a dúvida
+            apresentada, de acordo com esta Política de Privacidade. Os dados
+            acima são armazenados pela Talk to the Campus e associados à conta
+            do aluno.{' '}
           </p>
+
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            aspernatur, doloribus perferendis quam nemo eius laudantium
-            voluptatum. Assumenda, labore ipsum quo maxime expedita aliquid aut
-            cumque temporibus corrupti hic atque.
+            1.2 Dados coletados automaticamente: Quando o usuário acessa o site
+            coletamos determinados dados por meios automatizados, tais como:
+            Dados do sistema: Dados técnicos sobre o computador ou dispositivo
+            do usuário, tais como endereço de IP, tipo de dispositivo, tipo e
+            versão do sistema operacional, identificadores exclusivos do
+            dispositivo, navegador, idioma do navegador, domínio e outros dados
+            de sistemas e tipos de plataforma (“Dados do sistema”). Dados sobre
+            a utilização: Estatísticas de uso sobre as interações do usuário com
+            os site, tais como cursos acessados, tempo gasto nas páginas,
+            páginas visitadas, recursos utilizados, dados sobre os cliques, data
+            e hora e outros dados relacionados ao uso dos Serviços (“Dados de
+            uso”) por parte do usuário. Dados geográficos aproximados:
+            Localização geográfica aproximada, que inclui informações tais como
+            país, cidade e coordenadas geográficas, calculada com base no
+            endereço IP do usuário. Os dados listados acima são coletados por
+            meio de arquivos de log do servidor e tecnologias de rastreamento.
+            Os dados são armazenados pela Talk to the Campus e associados à
+            conta do usuário.{' '}
           </p>
+
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            aspernatur, doloribus perferendis quam nemo eius laudantium
-            voluptatum. Assumenda, labore ipsum quo maxime expedita aliquid aut
-            cumque temporibus corrupti hic atque.
+            2. Forma de coleta dos dados: Usamos cookies, serviços de análise e
+            pixels de provedores de publicidade para reunir os dados citados.
+            Algumas dessas ferramentas oferecem ao usuário a possibilidade de
+            optar por não permitir a coleta de dados.
           </p>
+
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            aspernatur, doloribus perferendis quam nemo eius laudantium
-            voluptatum. Assumenda, labore ipsum quo maxime expedita aliquid aut
-            cumque temporibus corrupti hic atque.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            aspernatur, doloribus perferendis quam nemo eius laudantium
-            voluptatum. Assumenda, labore ipsum quo maxime expedita aliquid aut
-            cumque temporibus corrupti hic atque.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            aspernatur, doloribus perferendis quam nemo eius laudantium
-            voluptatum. Assumenda, labore ipsum quo maxime expedita aliquid aut
-            cumque temporibus corrupti hic atque.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            aspernatur, doloribus perferendis quam nemo eius laudantium
-            voluptatum. Assumenda, labore ipsum quo maxime expedita aliquid aut
-            cumque temporibus corrupti hic atque.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            aspernatur, doloribus perferendis quam nemo eius laudantium
-            voluptatum. Assumenda, labore ipsum quo maxime expedita aliquid aut
-            cumque temporibus corrupti hic atque.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            aspernatur, doloribus perferendis quam nemo eius laudantium
-            voluptatum. Assumenda, labore ipsum quo maxime expedita aliquid aut
-            cumque temporibus corrupti hic atque.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            aspernatur, doloribus perferendis quam nemo eius laudantium
-            voluptatum. Assumenda, labore ipsum quo maxime expedita aliquid aut
-            cumque temporibus corrupti hic atque.
+            2.1 Cookies e ferramentas de coleta automatizadas A Talk to the
+            Campus e os provedores de serviços que atuam em nome da Talk to the
+            Campus (como o Google Analytics e anunciantes parceiros) usam
+            cookies, tags, scripts, links personalizados, rastros de
+            dispositivos ou navegadores para coleta automatizada de dados
+            (coletivamente, “Ferramentas de coleta de dados”), quando o usuário
+            acessa e usa o site. Utilizamos cookies (pequenos arquivos que os
+            sites enviam ao dispositivo do usuário para identificar, de forma
+            exclusiva, o navegador ou dispositivo do usuário ou para armazenar
+            dados no navegador do usuário) para analisar o uso do site,
+            personalizar a experiência do usuário, aprimorar a performance do
+            site e reconhecer o usuário quando ele retornar. Usamos pixels
+            (scripts que nos permitem medir as ações dos visitantes e usuários
+            que utilizam o site) para identificar se uma página foi visitada e
+            fazer anúncios de forma direcionada, excluindo usuários atuais de
+            determinadas campanhas promocionais. A Talk to the Campus usa os
+            seguintes tipos de cookies: Preferências: cookies que lembram dados
+            sobre o navegador e as configurações preferenciais do usuário que
+            afetam a aparência e o comportamento do site. Segurança: cookies
+            usadas para permitir que o usuário se conecte e acesse o site,
+            proteger contra acessos fraudulentos e ajudar a detectar e impedir
+            abuso ou uso não autorizado da conta do usuário. Funcional: cookies
+            que armazenam configurações funcionais (como o número de aulas
+            completadas). Estado da sessão: cookies que rastreiam as interações
+            do usuário com o site para nos ajudar a melhorar o site e a
+            experiência de navegação do usuário, lembrar os detalhes de acesso
+            do usuário e permitir o processamento das compras de Planos. Estas
+            são estritamente necessárias para que o site funcione corretamente.
+            Portanto, se o usuário as desativar, certos recursos não funcionarão
+            ou ficarão indisponíveis. Alguns parceiros terceirizados que
+            fornecem determinados recursos em nosso site podem também usar
+            objetos de armazenamento local para coletar e armazenar dados.
           </p>
         </div>
       </section>
